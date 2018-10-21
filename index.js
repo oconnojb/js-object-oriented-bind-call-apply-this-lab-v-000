baffc9c7d47aef225ec6cf9dcf26b284f7f95118
John O'Connor <oconnojb@gmail.com>
2018-10-21 21:14:38 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ function justInvoke(fn) {
 function setThisWithCall(fn, thisValue, arg) {
   return fn.call(thisValue, arg)
 }
+
+function setThisWithApply(fn, t, args) {
+  return fn.call(t, args)
+}
