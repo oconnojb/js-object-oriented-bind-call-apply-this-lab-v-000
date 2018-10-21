503e429fa6c410f2dd8fa9e99718e4ce48f1a270
John O'Connor <oconnojb@gmail.com>
2018-10-21 21:20:40 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 //Your code here
+function justInvoke(fn) {
+  return fn();
+}
+
+function setThisWithCall(fn, thisValue, arg) {
+  return fn.call(thisValue, arg)
+}
+
+function setThisWithApply(fn, t, args) {
+  return fn.apply(t, args)
+}
+
+function returnNewFunctionOf(fn, thisValue) {
+  let newFn = fn.bind(thisValue);
+  return newFn;
+}
